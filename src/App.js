08f7de6ce8a8368fafc64777e694ceb32d580f9f
Oliverizzle <trigmogi@gmail.com>
2022-06-22 21:27:38 -0500

    hmmm

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import { Analytics, AmazonPersonalizeProvider, AWSKinesisProvider } from '@aws-amplify/analytics';
 import awsconfig from './aws-exports';
+// Amplify.configure(awsconfig);
+
+
+Analytics.autoTrack('pageView', {
+  enable: true,
+  type: 'SPA'
+});
+
+Analytics.autoTrack('event', {
+  enable: true
+});
 
 
 Analytics.addPluggable(new AWSKinesisProvider());
+
+// Analytics.configure({
+//   AmazonPersonalize: {
+//     trackingId: 'fdsfsf', // TODO: Set to Personalize Tracking ID, e.g.: 'efd7edf5-7ddc-4732-b4b3-0374c6b721e9
+//     region: 'us-east-1'
+//   },
+//   AWSKinesis: {
+//     region: 'us-east-1'
+//   }
+// });
 
 Analytics.configure({
   AWSKinesis: {
