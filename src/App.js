288d5a6642c4dbae97178babdd9809495a0923ab
Oliverizzle <trigmogi@gmail.com>
2022-06-22 20:46:14 -0500

    stream

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         "safasf": 'gdsgd',
         timestamp: new Date()
     },
-    streamName: 'work-dev' 
+    streamName: 'ampplzKinesis-dev' 
 }, 'AWSKinesis')
 
   return (
